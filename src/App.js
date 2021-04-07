ef620ff3eac196847fca53df4918447c0e25c08c
HunCage <gkipeter@gmail.com>
2021-04-07 11:55:53 +0200

    list added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, NavLink, Redirect, Route, Switch } from "react-router-dom";
 import './App.css';
-
+import { InstrumentListPage } from "./InstrumentListPage";
 
 function App() {
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <span className="nav-link">Instruments</span>
-          </li>
-          <li className="nav-item">
-            <span className="nav-link">New Instruments</span>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <BrowserRouter>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink to={'/'} activeClassName="active" exact>
+                <span className="nav-link">Instruments</span>
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to={'/new-instrument'} activeClassName="active">
+                <span className="nav-link">New Instrument</span>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      <Switch>
+        <Route path="/" exact component={ InstrumentListPage } />
+
+        <Route path="/instrument/:instrumentId">
+          Instrument subpage
+        </Route>
+        <Route path="/new-instrument">
+          New Instrument add
+        </Route>
+        <Redirect to={"/"} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
